5bc79f46cdbc93188c1fc47d73c02bba47ac3d07
Lusheng Ji <lji@research.att.com>
2017-08-29 14:20:46 +0000

    Update LICENSE.txt, pom.xml's, etc
    Issue-Id: DCAEGEN2-86
    Change-Id: Ia0cda2a83eab6031d4e41d9fdf86bb2891132d5a
    Signed-off-by: Lusheng Ji <lji@research.att.com>

---
+++ mvn-phase-script.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# ================================================================================
+# Copyright (c) 2017 AT&T Intellectual Property. All rights reserved.
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============LICENSE_END=========================================================
+#
+# ECOMP is a trademark and service mark of AT&T Intellectual Property.
+
+
+echo "running script: [$0] for module [$1] at stage [$2]"
+
+echo "=> Prepare environment "
+#env
+
+TIMESTAMP=$(date +%C%y%m%dT%H%M%S) 
+export BUILD_NUMBER="${TIMESTAMP}"
+
+# expected environment variables 
+if [ -z "${MVN_NEXUSPROXY}" ]; then
+    echo "MVN_NEXUSPROXY environment variable not set.  Cannot proceed"
+    exit
+fi
+MVN_NEXUSPROXY_HOST=$(echo "$MVN_NEXUSPROXY" |cut -f3 -d'/' | cut -f1 -d':')
+echo "=> Nexu Proxy at $MVN_NEXUSPROXY_HOST, $MVN_NEXUSPROXY"
+
+# use the version text detect which phase we are in in LF CICD process: verify, merge, or (daily) release
+
+# mvn phase in life cycle 
+MVN_PHASE="$2"
+
+case $MVN_PHASE in
+clean)
+  echo "==> clean phase script"
+  ;;
+generate-sources)
+  echo "==> generate-sources phase script"
+  ;;
+compile)
+  echo "==> compile phase script"
+  ;;
+test)
+  echo "==> test phase script"
+  ;;
+package)
+  echo "==> package phase script"
+  ;;
+install)
+  echo "==> install phase script"
+  ;;
+deploy)
+  echo "==> deploy phase script"
+  ;;
+*)
+  echo "==> unprocessed phase"
+  ;;
+esac
+
