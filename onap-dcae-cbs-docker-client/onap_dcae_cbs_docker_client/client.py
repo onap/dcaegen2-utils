b54236bb49c0b932678cb5e9e227d60553dfde4b
Tommy Carpenter <tommy@research.att.com>
2019-06-19 14:54:53 +0000

    Move to k8s service names, exceptions
    Issue-ID: DCAEGEN2-1537
    Change-Id: Ibb7bd0233b5de9eb853f3516ba0907e148eae21d
    Signed-off-by: Tommy Carpenter <tommy@research.att.com>

--- onap-dcae-cbs-docker-client/onap_dcae_cbs_docker_client/client.py
+++ onap-dcae-cbs-docker-client/onap_dcae_cbs_docker_client/client.py
@@ -1,5 +1,5 @@
 # ================================================================================
-# Copyright (c) 2017-2018 AT&T Intellectual Property. All rights reserved.
+# Copyright (c) 2017-2019 AT&T Intellectual Property. All rights reserved.
 # ================================================================================
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,50 +18,16 @@
 
 import json
 import os
-import logging
 import requests
+from onap_dcae_cbs_docker_client import get_module_logger
+from onap_dcae_cbs_docker_client.exceptions import ENVsMissing, CantGetConfig, CBSUnreachable
 
-LOGGER = logging.getLogger().getChild(__name__)
 
-
-class ENVsMissing(Exception):
-    """
-    Exception to represent critical ENVs are missing
-    """
-    pass
+logger = get_module_logger(__name__)
 
 
 #########
 # HELPERS
-
-
-def _get_uri_from_consul(consul_url, name):
-    """
-    Call consul's catalog
-    TODO: currently assumes there is only one service with this hostname
-    """
-    url = "{0}/v1/catalog/service/{1}".format(consul_url, name)
-    LOGGER.debug("Trying to lookup service: {0}".format(url))
-    res = requests.get(url)
-    res.raise_for_status()
-    services = res.json()
-    return "http://{0}:{1}".format(services[0]["ServiceAddress"], services[0]["ServicePort"])
-
-
-def _get_envs():
-    """
-    Returns hostname, consul_host.
-    If the necessary ENVs are not found, this is fatal, and raises an exception.
-    """
-    try:
-        hostname = os.environ["HOSTNAME"]  # this is the name of the component itself
-        consul_host = os.environ["CONSUL_HOST"]  # this is the host of consul itself
-        cbs_name = os.environ["CONFIG_BINDING_SERVICE"]  # this is the name under which the CBS is registered in Consul.
-    except KeyError as e:
-        raise ENVsMissing("Required ENV Variable {0} missing".format(e))
-    return hostname, consul_host, cbs_name
-
-
 def _get_path(path):
     """
     This call does not raise an exception if Consul or the CBS cannot complete the request.
@@ -69,31 +35,40 @@ def _get_path(path):
     It could be a temporary network outage. Call me again later.
 
     It will raise an exception if the necessary env parameters were not set because that is irrecoverable.
-    This function is called in my /heatlhcheck, so this will be caught early.
     """
-
-    config = {}
-
-    hostname, consul_host, cbs_name = _get_envs()
-
-    # not sure how I as the component developer is supposed to know consul port
-    consul_url = "http://{0}:8500".format(consul_host)
-
     try:
-        # get my config
-        cbs_url = _get_uri_from_consul(consul_url, cbs_name)
+        hostname = os.environ["HOSTNAME"]  # this is the name of the component itself
+        # in most cases, this is the K8s service name which is a resolavable DNS name
+        # if running outside k8s, this name needs to be resolvable by DNS via other means.
+        cbs_resolvable_hostname = os.environ["CONFIG_BINDING_SERVICE"]
+    except KeyError as e:
+        raise ENVsMissing("Required ENV Variable {0} missing".format(e))
+
+    # TODO: https
+    cbs_url = "http://{0}:10000".format(cbs_resolvable_hostname)
+
+    # get my config
+    try:
         my_config_endpoint = "{0}/{1}/{2}".format(cbs_url, path, hostname)
         res = requests.get(my_config_endpoint)
-
         res.raise_for_status()
         config = res.json()
-        LOGGER.info("get_config returned the following configuration: {0}".format(
-            json.dumps(config)))
-    except requests.exceptions.HTTPError:
-        LOGGER.error("in get_config, the config binding service endpoint %s was not reachable. Error code: %d, Error text: %s", my_config_endpoint, res.status_code, res.text)
-    except Exception as exc:
-        LOGGER.exception(exc)
-    return config
+        logger.debug(
+            "get_config returned the following configuration: %s using the config url %s",
+            json.dumps(config),
+            my_config_endpoint,
+        )
+        return config
+    except requests.exceptions.HTTPError:  # this is thrown by raise_for_status
+        logger.error(
+            "The config binding service endpoint %s returned a bad status. code: %d, text: %s",
+            my_config_endpoint,
+            res.status_code,
+            res.text,
+        )
+        raise CantGetConfig()
+    except requests.exceptions.ConnectionError:  # this is thrown if requests.get cant even connect to the endpoint
+        raise CBSUnreachable()
 
 
 #########
